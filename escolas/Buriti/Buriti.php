fb2b46cd07094a2392751d8dbae79815d8b3d21c
unknown <elystonpintodecastro@hotmail.com>
2023-10-15 14:11:12 -0400

    commit pastas e fotos

---
+++ escolas/Buriti/Buriti.php
@@ -0,0 +1,113 @@
+<?php
+echo('<!DOCTYPE html>');
+echo('<html>');
+echo('	<head>');
+echo('		<meta charset="utf-8">');
+echo('                <link rel="stylesheet" type="text/css" href="../../config.css">');
+echo('	</head>');
+echo('	<body>');
+echo('		<div>');
+echo('			<div>');
+echo('				<h1 align="center" class="container">ESCOLA ESTADUAL BURITI</h1>');
+echo('			</div>');
+echo('			<div class="fundo_colorido">');
+echo('				<h2>Breve história do nome:</h2>');
+echo('				<p>O buriti ou murici (nome cientifico: Mauritia flexuosa) é uma planta de ampla distribuição no território nacional. Pode alcançar até 30 metros de altura e ter um caule com espessura de até 50 cm de diâmetro. ... A casca dura do buriti é uma proteção natural contra predadores e contra a entrada de água. </p>');
+echo('			<div>');
+echo('				<h2>ENDEREÇO</h2>');
+echo('				<p>Está localizada na Rua Antonio Pinheiro Galvão, 837. Bairro Buritis.</p>');
+echo('			</div>');
+echo('			</div>');
+echo('			<div class="fundo_colorido">');
+echo('				<h2>INAUGURADO EM:</h2>');
+echo('				<h3>ESTRUTURA FÍSICA</h3>');
+echo('				<table border="5">');
+echo('					<tr>');
+echo('						<th>Vagas</th>');
+echo('						<th>Salas</th>');
+echo('						<th>Turmas</th>');
+echo('					</tr>');
+echo('					<tr>');
+echo('						<th>250</th>');
+echo('						<th>10</th>');
+echo('						<th>15</th>');
+echo('					</tr>');
+echo('				</table>');
+echo('				<h3>IDEB: 75%</h3>');
+echo('				<h3>MODALIDADE DE ENSINO: FUNDAMENTAL e educação especial</h3>');
+echo('<h3>TURNO: matutino</h3>
+				<div class="colorido">
+					<h3><a href="fotos.html">clique aqui para acessar a coleção de livros da biblioteca</a></h3>
+				</div>
+			</div>');
+echo('<div class="menu">
+				<h2>Comentários dos usuarios</h3>
+                                
+                                
+				<div>
+					<label for="nome">nome:</label>
+					<input type="text" id="nome"/>
+				</div>
+				<div>
+					<label for="comentario">poste seu depoimento sobre a escola:</label>
+					<textarea></textarea>
+				</div>
+				<div>
+					<button id="enviar" type="submit">Enviar</button>
+				</div>
+				
+			</div>');
+echo('</div>');
+echo('</body>');
+echo('</html>');
+
+
+
+
+
+
+
+
+
+
+
+
+	
+
+
+
+
+
+
+
+
+			
+
+			
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+				
+			
+			
+		
+		
+		
+		
+		
+		
+	
